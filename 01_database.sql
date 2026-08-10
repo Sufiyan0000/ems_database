c0b8a1f93d6302689a3fc350bc0d12d98dc7724a
Sufiyan0000 <alimdsufiyan89@gmail.com>
2026-08-10 12:54:39 +0530

    Create schools table

---
+++ 01_database.sql
@@ -0,0 +1,11 @@
+-- ======================================
+-- EMS Database
+-- Version : 1.0
+-- Author  : Muhammad Sufiyan Ali
+-- ======================================
+
+CREATE DATABASE IF NOT EXISTS ems_db
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+USE ems_db;
---
+++ 02_tables/01_school.sql
@@ -0,0 +1,26 @@
+-- ======================================================
+-- File       : 01_school.sql
+-- Module     : School
+-- Description: Stores school information
+-- ======================================================
+
+CREATE TABLE schools (
+
+    school_id VARCHAR(36) PRIMARY KEY,
+
+    name VARCHAR(150) NOT NULL,
+
+    address VARCHAR(255) NOT NULL,
+
+    email VARCHAR(150) NOT NULL,
+
+    phone VARCHAR(15) NOT NULL,
+
+    logo VARCHAR(255),
+
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    ON UPDATE CURRENT_TIMESTAMP
+
+);
