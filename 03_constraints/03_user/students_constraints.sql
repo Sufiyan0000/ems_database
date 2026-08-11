5804580ad0ae26bf98fd0cc9c86e2f5afd7ae14e
Sufiyan0000 <alimdsufiyan89@gmail.com>
2026-08-11 11:56:06 +0530

    add constraints for students table

---
+++ 03_constraints/03_user/students_constraints.sql
@@ -0,0 +1,33 @@
+-- ======================================================
+-- File       : students_constraints.sql
+-- Module     : People
+-- Description: Foreign key and check constraints
+--              for students
+-- ======================================================
+
+-- ======================================================
+-- Foreign Keys
+-- ======================================================
+
+ALTER TABLE students
+ADD CONSTRAINT fk_students_school
+FOREIGN KEY (school_id)
+REFERENCES school(school_id)
+ON UPDATE CASCADE
+ON DELETE RESTRICT;
+
+-- ======================================================
+-- Check Constraints
+-- ======================================================
+
+ALTER TABLE students
+ADD CONSTRAINT chk_students_dob
+CHECK (
+    dob <= CURRENT_DATE
+);
+
+ALTER TABLE students
+ADD CONSTRAINT chk_students_join_date
+CHECK (
+    join_date >= dob
+);
