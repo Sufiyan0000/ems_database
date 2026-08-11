f8ca3f9ce77a88eba093fdbb5eddd96f72cd0429
Sufiyan0000 <alimdsufiyan89@gmail.com>
2026-08-11 12:59:02 +0530

    update the transport_vehicle table by removing driver_name and driver_photo with driver_id

--- 02_tables/06_transport/transport_vehicles.sql
+++ 02_tables/06_transport/transport_vehicles.sql
@@ -14,10 +14,8 @@ CREATE TABLE transport_vehicles (
 
     vehicle_no VARCHAR(20) NOT NULL,
 
-    driver_name VARCHAR(100) NOT NULL,
-
-    driver_phone VARCHAR(20) NOT NULL,
-
+    driver_id VARCHAR(36) NOT NULL,
+    
     capacity INT NOT NULL,
 
     status ENUM(
